c8152474a3c211c460f7eb1633633d98f028a708
Raimanovv <rustam.raimanovv@gmail.com>
2022-02-03 16:02:15 +0500

    03.02.22

---
+++ Basic - часть I/66.py
@@ -0,0 +1,5 @@
+# Напишите программу на Python для расчета индекса массы тела.
+
+height = float(input("Input your height in meters: "))
+weight = float(input("Input your weight in kilogram: "))
+print("Your body mass index is: ", round(weight / (height * height), 2))
---
+++ Basic - часть I/70+.py
@@ -0,0 +1,7 @@
+# Напишите программу на Python для сортировки файлов по дате.
+
+import glob
+import os
+files = glob.glob("*.py")
+files.sort(key=os.path.getmtime)
+print("\n".join(files))
