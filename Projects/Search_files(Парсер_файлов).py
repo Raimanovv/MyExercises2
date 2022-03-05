4813b084a5bb43269228473c6c3f84441fccbfed
elina <rustam.raimanovv@gmail.com>
2022-03-05 22:30:57 +0500

    05.03.2022

--- Projects/Search_files(Парсер_файлов).py
+++ Projects/Search_files(Парсер_файлов).py
@@ -9,7 +9,7 @@ PATH_FOR_COPY = input('Куда копировать файлы?\n')
 def search():
     for adress, dirs, files in os.walk(input('Введите путь старта\n')):  # Выдает по картежу (путь, [папки], [файлы])
         for file in files:
-            if file.endswith('.txt'):  # Если файл заканчивается на '.txt', то True
+            if file.endswith('.txt') and '$' not in file:  # Если файл заканчивается на '.txt', то True
                 yield os.path.join(adress, file)
                 '''
                 # yield делает из функции объект генератор
@@ -31,6 +31,16 @@ def read_from_pathtxt(path):
 def copy(path):
     file_name = path.split('\\')[-1]  # Путь делим по символу '\' и из полученного списка берем последний элемент
     # Это имя файла с расширением
+    count = 1
+    while True:
+        if os.path.isfile(os.path.join(PATH_FOR_COPY, file_name)):  # Проверяет существует ли по такому пути к-т файл
+            if f'({count - 1})' in file_name:  # Если мы уже создавали такой файл, то True
+                file_name = file_name.replace(f'({count - 1})', '')  # Удаляет ненужное число в ()
+            file_name = f'({count}).'.join(file_name.split('.'))
+            # Объединяет добавленный счетчик в '().' с помощью '(count).' после разделения по '.'
+            count += 1
+        else:
+            break
 
     shutil.copyfile(path, os.path.join(PATH_FOR_COPY, file_name))  # os.path.join(Путь, ИмяНовогоФайлаКудаКопируем)
     # shutil.copyfile(ПутьГдеЛежитОригинал, Путь'\'ИмяНовогоФайлаКудаКопируем)
