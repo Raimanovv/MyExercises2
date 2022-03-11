40652acf581be59fa3844fbf3818144aaa136e46
Raimanovv <rustam.raimanovv@gmail.com>
2022-03-11 14:34:16 +0500

    11.03.22

--- Function/lambda()/map(lambda(filter)).py
+++ Function/lambda()/map(lambda(filter)).py
@@ -1,4 +1,13 @@
-# Примеры работы с lambda
+# Пример работы с lambda
+
+# аналог def
+# можно писать все, что допустимо после return в def
+# не выполняется до вызова ()
+# можно писать без лямбд
 
 nn = list(range(10))
 print(list(map(lambda y: y ** 2, filter(lambda x: x % 2 == 0, nn))))
+
+# Можно заменить на ...
+
+#print(list(i ** 2 for i in nn if i % 2 == 0))
--- Function/lambda()/sorted().py
+++ Function/lambda()/sorted().py
@@ -1,4 +1,4 @@
-# Примеры работы с lambda
+# Пример работы с lambda
 
 a_dict = {'a': 3, 'f': 2, 'b': 6}
 print(sorted(a_dict.items(), key=lambda x: x[1]))
