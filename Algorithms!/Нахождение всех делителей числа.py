f9e8a45b80be5cf13e987d4f3af1ac86d049efcd
elina <rustam.raimanovv@gmail.com>
2022-03-14 13:05:40 +0500

    14.03.2022

---
+++ Algorithms!/Нахождение всех делителей числа.py
@@ -0,0 +1,16 @@
+# https://stepik.org/lesson/296955/step/1?unit=278683
+# Нахождение всех делителей числа
+# Лучшее решение
+
+n = int(input('Введите число: '))
+i = 1
+a = []
+
+while i * i <= n:
+    if n % i == 0:
+        a.append(i)
+        if i != n // i:
+            a.append(n // i)
+    i += 1
+a.sort()
+print(a)
