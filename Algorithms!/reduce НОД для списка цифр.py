9e349b231b47c8d08fa986deb765a2db89927a2c
elina <rustam.raimanovv@gmail.com>
2022-03-22 20:49:39 +0500

    22.03.2022

---
+++ Algorithms!/reduce НОД для списка цифр.py
@@ -0,0 +1,19 @@
+from functools import reduce
+
+
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
+
+
+n = int(input())
+ll = list()
+
+for _ in range(n):
+    i = int(input())
+    ll.append(i)
+
+x = reduce(gcd, ll)
+
+print(x)
