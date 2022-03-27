6222949dbd0a3e5c58529d9fe56ac53f8f99c5eb
elina <rustam.raimanovv@gmail.com>
2022-03-27 15:35:24 +0500

    27.03.2022

--- Algorithms!/Fibonacci (Быстро!!!!).py
+++ Algorithms!/Fibonacci (Быстро!!!!).py
@@ -1,15 +1,17 @@
 t = [0] * 1000
 
+
 def f(n):
     if n <= 1:
         return n
     else:
         if t[n - 1] == 0:
             t[n - 1] = f(n - 1)
-        if t[n - 2] == 0:
-            t[n - 2] = f(n - 2)
+        if t[n - 2] == 0:  # ????? Можно ведь без этого
+            t[n - 2] = f(n - 2)  # ????? Можно ведь без этого
         t[n] = t[n - 1] + t[n - 2]
         return t[n]
+
 
 print(f(10))
 print(t)
