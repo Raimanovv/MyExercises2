edef902d9ce8560e5e35c0a7479e82f77e611018
elina <rustam.raimanovv@gmail.com>
2022-03-27 15:18:19 +0500

    27.03.2022

---
+++ Algorithms!/Fibonacci (Быстро!!!).py
@@ -0,0 +1,15 @@
+t = [0] * 1000
+
+def f(n):
+    if n <= 1:
+        return n
+    else:
+        if t[n - 1] == 0:
+            t[n - 1] = f(n - 1)
+        if t[n - 2] == 0:
+            t[n - 2] = f(n - 2)
+        t[n] = t[n - 1] + t[n - 2]
+        return t[n]
+
+print(f(10))
+print(t)
